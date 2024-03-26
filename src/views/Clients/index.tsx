1b960733ed8a66e9e2b91b154f3b5f7cf81799d4
touseefali-visnext <touseef.ali@visnext.com>
2024-03-26 12:54:54 +0500

    Filter dropdown added

--- src/views/Clients/index.tsx
+++ src/views/Clients/index.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import { Menu, Transition } from '@headlessui/react';
 import ClientIcon from "../../assets/img/clientsIcon.png";
 import FilterIcon from "../../assets/img/filterIcon.png";
 import WordIcon from "../../assets/img/wordIcon.png";
@@ -7,19 +9,48 @@ import PdfIcon from "../../assets/img/pdfIcon.png";
 import EditIcon from "../../assets/img/editIcon.png";
 import DeleteIcon from "../../assets/img/deleteIcon.png";
 import SearchIcon from "../../assets/img/SearchIcon.png";
-import { useNavigate } from "react-router-dom";
 
 export default function Clients() {
-
   const navigate = useNavigate();
-
-  const data = new Array(19).fill({
-    badge: "Attention",
-    cName: "XYZ Hospital Trust",
-    city: "London",
-    primaryContract: "Patricia L. Dubose",
-    sites: "3",
+  const [showByBadge, setShowByBadge] = useState(false);
+  const [showByBadgeFilter, setShowByBadgeFilter] = useState({
+    attention: false,
+    expired: false,
   });
+
+  const tempData = new Array(19).fill({
+    badge: 'Attention',
+    cName: 'XYZ Hospital Trust',
+    city: 'London',
+    primaryContract: 'Patricia L. Dubose',
+    sites: '3',
+  });
+
+  const [data, setData] = useState(tempData);
+
+  const handleApplyFilters = (closeBox) => {
+    let dataT = tempData;
+    if (
+      (showByBadgeFilter.attention && showByBadgeFilter.expired) ||
+      (!showByBadgeFilter.attention && !showByBadgeFilter.expired)
+    ) {
+      setData(dataT);
+    } else if (showByBadgeFilter.attention && !showByBadgeFilter.expired) {
+      dataT = data.filter((curr) => curr.badge === "Attention");
+      setData(dataT);
+    } else {
+      dataT = data.filter((curr) => curr.badge === "Expired");
+      setData(dataT);
+    }
+    closeBox();
+  };
+
+  const handleClickClearFilter = () => {
+    setShowByBadge(false);
+    setShowByBadgeFilter({ attention: false, expired: false });
+    setData(tempData);
+  };
+
   return (
     <div className="py-6 px-8 bg-alice-blue-50">
       <div className="flex justify-between items-center">
@@ -40,10 +71,205 @@ export default function Clients() {
               <img src={ClientIcon} alt="icon" />
               Clients (799)
             </div>
-            <button className="flex items-center gap-2">
-              <img src={FilterIcon} alt="icon" />
-              Show Filters
-            </button>
+            <Menu as="div" className="relative inline-block text-left">
+              <div>
+                <Menu.Button className="inline-flex w-full items-center gap-x-1.5 rounded-md px-3 py-2 hover:bg-gray-50">
+                  <img src={FilterIcon} alt="icon" />
+                  Show Filters
+                </Menu.Button>
+              </div>
+
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+              >
+                <Menu.Items className="absolute left-0 z-10 py-5 mt-2 w-96 origin-top-right rounded-md bg-white shadow-xl border border-pacific-blue focus:outline-none">
+                  <div className="py-1">
+                    <Menu.Item>
+                      {({ close }) => (
+                        <div className="flex justify-between px-5">
+                          <div className="flex items-center gap-x-2">
+                            <img src={FilterIcon} alt="icon" />
+                            <span>Search Filters</span>
+                          </div>
+                          <div>
+                            <button
+                              className="py-1 px-4 border border-pacific-blue rounded-lg"
+                              onClick={handleClickClearFilter}
+                            >
+                              Clear
+                            </button>
+                          </div>
+                          <button
+                            className="py-1 px-4 border border-pacific-blue rounded-lg"
+                            onClick={() => handleApplyFilters(close)}
+                          >
+                            Filter
+                          </button>
+                        </div>
+                      )}
+                    </Menu.Item>
+                    <Menu.Item>
+                      {() => (
+                        <>
+                          <div
+                            onClick={() => setShowByBadge(!showByBadge)}
+                            className="hover:bg-gray-100 mt-5 px-9 py-2 text-sm flex items-center justify-between cursor-pointer"
+                          >
+                            <span>By Badge</span>
+                            {showByBadge ? (
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="currentColor"
+                                className="w-6 h-6"
+                              >
+                                <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  d="m4.5 15.75 7.5-7.5 7.5 7.5"
+                                />
+                              </svg>
+                            ) : (
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="currentColor"
+                                className="w-6 h-6"
+                              >
+                                <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  d="m19.5 8.25-7.5 7.5-7.5-7.5"
+                                />
+                              </svg>
+                            )}
+                          </div>
+                          <div className={showByBadge ? "block" : "hidden"}>
+                            <div className="flex items-center py-6 px-4 bg-hawkes-blue">
+                              <input
+                                id="default-checkbox"
+                                type="checkbox"
+                                value="expired"
+                                checked={showByBadgeFilter.expired}
+                                onChange={(e) =>
+                                  setShowByBadgeFilter({
+                                  ...showByBadgeFilter,
+                                  expired: e.target.checked,
+                                })
+                                }
+                                className="w-4 h-4 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                              />
+                              <label
+                                htmlFor="default-checkbox"
+                                className="ms-2 text-sm"
+                              >
+                                Expired
+                              </label>
+                            </div>
+                            <div className="flex items-center py-6 px-4 bg-hawkes-blue">
+                              <input
+                                id="default-checkbox"
+                                type="checkbox"
+                                value="attention"
+                                checked={showByBadgeFilter.attention}
+                                onChange={(e) =>
+                                  setShowByBadgeFilter({
+                                  ...showByBadgeFilter,
+                                  attention: e.target.checked,
+                                })
+                                }
+                                className="w-4 h-4 bg-gray-100 border-gray-300 rounded focus:ring-pale-cornfower-blue"
+                              />
+                              <label
+                                htmlFor="default-checkbox"
+                                className="ms-2 text-sm"
+                              >
+                                Attention
+                              </label>
+                            </div>
+                          </div>
+                        </>
+                      )}
+                    </Menu.Item>
+                    <Menu.Item>
+                      {() => (
+                        <div className="hover:bg-gray-100 mt-5 px-9 py-2 text-sm flex items-center justify-between cursor-pointer">
+                          <span>By Name</span>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-6 h-6"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="m19.5 8.25-7.5 7.5-7.5-7.5"
+                            />
+                          </svg>
+                        </div>
+                      )}
+                    </Menu.Item>
+                  </div>
+                  <div className="py-1">
+                    <Menu.Item>
+                      {() => (
+                        <div className="hover:bg-gray-100 mt-5 px-9 py-2 text-sm flex items-center justify-between cursor-pointer">
+                          <span>By Year</span>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-6 h-6"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="m19.5 8.25-7.5 7.5-7.5-7.5"
+                            />
+                          </svg>
+                        </div>
+                      )}
+                    </Menu.Item>
+                    <Menu.Item>
+                      {() => (
+                        <div className="hover:bg-gray-100 mt-5 px-9 py-2 text-sm flex items-center justify-between cursor-pointer">
+                          <span>By Number of Sites</span>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-6 h-6"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="m19.5 8.25-7.5 7.5-7.5-7.5"
+                            />
+                          </svg>
+                        </div>
+                      )}
+                    </Menu.Item>
+                  </div>
+                </Menu.Items>
+              </Transition>
+            </Menu>
           </div>
           <div className="flex items-center justify-end gap-3">
             <img src={ExcelIcon} alt="icon" />
@@ -118,7 +344,7 @@ export default function Clients() {
                       <img
                         src={EditIcon}
                         alt="icon"
-                        onClick={() => navigate('/client-list/edit/1')}
+                        onClick={() => navigate("/client-list/edit/1")}
                         className="bg-hawkes-blue p-1 rounded-lg"
                       />
                       <img
